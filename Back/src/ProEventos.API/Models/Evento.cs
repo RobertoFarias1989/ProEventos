41545ac1bcea82d3b6864f61275eef3c45a8d87b
Roberto Farias <robertosf1989@gmail.com>
2023-11-15 14:36:07 -0300

    first commit

--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -8,6 +8,6 @@ namespace ProEventos.API.Models
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }
-        public string ImagemUrl { get; set; }
+        public string ImagemUrl { get; set; } 
     }
 }
